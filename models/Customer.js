83ab392565641e3421efcf9a1b400140d0723660
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-10-01 13:27:05 +0200

    implement Customer schema

---
+++ models/Customer.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: [true, "Phone is required"],
+    trim: true,
+  },
+  isGold: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
