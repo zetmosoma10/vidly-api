804f453d62cc52711409c7a99a68275e0d5a878b
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-10-01 13:18:03 +0200

    implement customer route utl endpoint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const genresRoutes = require("./routes/genresRoutes");
-
+const customerRoutes = require("./routes/customerRoutes");
 const app = express();
 
 app.use(express.json());
@@ -9,5 +9,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use("/api/genres", genresRoutes);
+app.use("/api/customers", customerRoutes);
 
 module.exports = app;
