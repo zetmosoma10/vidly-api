92005ea7b537819a19a38f116b30cb381cf4e853
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-10-02 17:32:29 +0200

    implement get rentals handler

--- controllers/rentalsController.js
+++ controllers/rentalsController.js
@@ -2,7 +2,15 @@ const { Rental, validateRentals } = require("../models/Rentals");
 
 exports.createRental = async (req, res) => {};
 
-exports.getRentals = async (req, res) => {};
+exports.getRentals = async (req, res) => {
+  const rentals = await Rental.find().sort("-dateOut");
+
+  res.status(200).json({
+    status: "success",
+    count: rentals.length,
+    data: { rentals },
+  });
+};
 
 exports.getRental = async (req, res) => {};
 
