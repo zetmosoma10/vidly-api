812955bccb7ad500c2d463526debd1cd06090692
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-10-02 17:19:33 +0200

    create Retal schema

---
+++ models/Rentals.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const rentalsSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+        minLength: 3,
+        maxLength: 50,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: [true, "Phone is required"],
+        minLength: 5,
+        maxLength: 50,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: [true, "Title is required"],
+        trim: true,
+        minLength: 3,
+        maxLength: 255,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
+
+const Rental = mongoose.model("Rental", rentalsSchema);
+
+const validateRentals = (req) => {
+  const schema = Joi.object({
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return error.details.map((err) => err.message).join(", ");
+  }
+
+  return null;
+};
+
+module.exports.Rental = Rental;
+module.exports.validateRentals = validateRentals;
