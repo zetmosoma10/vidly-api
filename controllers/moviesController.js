f697538a271bc9ac1825cf6aada1a9ea6281392f
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-10-02 15:33:47 +0200

    implement create movie handler

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,6 +1,38 @@
-const { Movie } = require("../models/Movie");
+const { Movie, validateMovie } = require("../models/Movie");
+const { Genre } = require("../models/Genre");
 
-exports.createMovie = async (req, res) => {};
+exports.createMovie = async (req, res) => {
+  const err = validateMovie(req);
+  if (err) {
+    return res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid genre",
+    });
+  }
+
+  const movie = await Movie.create({
+    title: req.body.title,
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: { movie },
+  });
+};
 
 exports.getMovies = async (req, res) => {};
 
--- models/Movie.js
+++ models/Movie.js
@@ -7,16 +7,19 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: [true, "Title is required"],
     trim: true,
+    unique: true,
   },
   numberInStock: {
     type: Number,
     default: 0,
     min: 0,
+    max:255
   },
   dailyRentalRate: {
     type: Number,
-    min: 0,
     default: 0,
+    min: 0,
+    max:255
   },
   genre: {
     type: genreSchema,
@@ -26,4 +29,22 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model("Movie", movieSchema);
 
+// ! VALIDATE MOVIE (Joi)
+const validateMovie = (req) => {
+  const schema = Joi.object({
+    title: Joi.string().min(3).max(255).required().trim(),
+    numberInStock: Joi.number().min(0).default(0),
+    dailyRentalRate: Joi.number().min(0).default(0),
+    genreId: Joi.string().required()
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return error.details.map((err) => err.message).join(", ");
+  }
+
+  return null;
+};
+
 module.exports.Movie = Movie;
+module.exports.validateMovie = validateMovie;
