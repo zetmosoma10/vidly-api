407582e0f5f7e439cf4f45da3c64fa2c2a2b5c9f
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-10-02 18:04:33 +0200

    implement create rental handler

--- controllers/rentalsController.js
+++ controllers/rentalsController.js
@@ -1,6 +1,60 @@
 const { Rental, validateRentals } = require("../models/Rentals");
+const { Customer } = require("../models/Customer");
+const { Movie } = require("../models/Movie");
 
-exports.createRental = async (req, res) => {};
+exports.createRental = async (req, res) => {
+  const err = validateRentals(req);
+  if (err) {
+    return res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid customer",
+    });
+  }
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid movie",
+    });
+  }
+
+  if (movie.numberInStock === 0) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Movie not in stock",
+    });
+  }
+
+  const rental = await Rental.create({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    },
+  });
+
+  movie.numberInStock--;
+  movie.save();
+
+  res.status(200).json({
+    status: "success",
+    data: { rental },
+  });
+};
 
 exports.getRentals = async (req, res) => {
   const rentals = await Rental.find().sort("-dateOut");
