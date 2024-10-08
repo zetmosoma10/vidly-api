b70e871d8dfc912985144de09bcc70a45a654ec8
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-10-08 11:48:35 +0200

    implement createUser functionality

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,29 @@
-const User = require("../models/User");
+const { User, validateUser } = require("../models/User");
 
-exports.createUser = async (req, res) => {};
+exports.createUser = async (req, res) => {
+  const err = validateUser(req);
+  if (err) {
+    return res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error,
+    });
+  }
+};
